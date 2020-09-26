a5c8d4ca6c1fdce8abacdd446ffb2b7b51006dc3
David Villegas <david.villegasg7@gmail.com>
2020-09-26 18:14:02 -0400

    handle exeptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_exception
+  rescue_from Net::SMTPAuthenticationError, with: :handle_smtp_auth
+
+  private
+
+  def handle_exception
+    redirect_back fallback_location: home_index_path, alert: "Registro no encontrado"
+  end
+
+  def handle_smtp_auth
+    redirect_back fallback_location: home_index_path, alert: "Hubo un problema de conexión con nuestro Servidor de Correos, Por Favor intente mas tarde."
+  end
 end
